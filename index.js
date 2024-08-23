fc96c5851568fdc62a8eec3db91f033c1e61ef2c
AswanthAchu <pnkaswanthachu546@gmail.com>
2024-08-23 16:45:01 +0530

    Automation Backend almost completed.

---
+++ index.js
@@ -0,0 +1,229 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const {
+  emailTemplate1,
+  emailTemplate2,
+  emailTemplate3,
+} = require("./util/template");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+// app.options("*", cors(corsOptions));
+// app.use(cors("*"));
+app.use(bodyParser.json());
+const accountSid = "AC2776c6a162fda0e2ea3a7843d3c61e17";
+const authToken = "3d65b952958c34ad8f065ab374b7a67d";
+const client = require("twilio")(accountSid, authToken);
+
+const otpStore = new Map();
+
+const generateOTP = () => {
+  return Math.floor(100000 + Math.random() * 900000).toString();
+};
+
+function formatPhoneNumber(phoneNumber) {
+  if (phoneNumber.length === 10) {
+    phoneNumber = "91" + phoneNumber;
+  } else if (phoneNumber.length === 12 && phoneNumber.startsWith("91")) {
+    // Phone number is already in the correct format
+  } else {
+    throw new Error("Invalid phone number length");
+  }
+
+  let country = phoneNumber.slice(0, 2);
+  let part1 = phoneNumber.slice(2, 7);
+  let part2 = phoneNumber.slice(7, 12);
+
+  return `+${country} ${part1} ${part2}`;
+}
+
+app.post("/api/request-otp", async (req, res) => {
+  try {
+    const { phone } = req.body;
+
+    const otp = generateOTP();
+
+    otpStore.set(phone, { otp, expires: Date.now() + 300000 });
+
+    let formattedNumber = await formatPhoneNumber(phone.trim());
+
+    console.log(formattedNumber);
+
+    client.messages
+      .create({
+        body: `Your IPCS Global Verification code is  ${otp}`,
+        from: "+12283356266",
+        to: formattedNumber,
+      })
+      .then((message) =>
+        res.status(200).json({ success: true, message: "OTP sent!" })
+      )
+      .catch((err) => {
+        console.log(err);
+
+        res.status(500).json({ success: false, message: err.message });
+      });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
+
+app.post("/api/verify-otp", (req, res) => {
+  const { otp, phone, name, email, qualification, location, form } = req.body;
+
+  const record = otpStore.get(phone);
+
+  if (record && record.otp === otp && record.expires > Date.now()) {
+    otpStore.delete(phone);
+
+    let emailHtml;
+    if (form === "Register") {
+      emailHtml = emailTemplate1
+        .replace("{{name}}", name)
+        .replace("{{email}}", email)
+        .replace("{{phone}}", phone)
+        .replace("{{qualification}}", qualification)
+        .replace("{{location}}", location);
+    } else if (form === "Offer") {
+      emailHtml = emailTemplate2
+        .replace("{{name}}", name)
+        .replace("{{phone}}", phone);
+    } else if (form === "Brochure") {
+      emailHtml = emailTemplate3
+        .replace("{{name}}", name)
+        .replace("{{email}}", email)
+        .replace("{{phone}}", phone);
+    }
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "ipcsglobalindia@gmail.com",
+        pass: "nnpd xhea roak mbbk",
+      },
+    });
+
+    const mailOptions = {
+      from: "ipcsglobalindia@gmail.com",
+      to: ["ipcsdeveloper@gmail.com"],
+      subject: "New Lead Form Submission on ",
+      html: emailHtml,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error sending email:", error);
+        res.status(500).send("Error sending email");
+      } else {
+        console.log("Email sent:", info.response);
+        res.status(200).send("Email sent successfully");
+      }
+    });
+
+  } else {
+    res
+      .status(400)
+      .json({ success: false, message: "Invalid OTP or OTP expired!" });
+  }
+});
+
+app.post("/api/resend-otp", async (req, res) => {
+  try {
+    const { phone } = req.body;
+
+    const existingOtpData = otpStore.get(phone);
+    if (existingOtpData && existingOtpData.expires > Date.now()) {
+      return res.status(429).json({
+        success: false,
+        message: "OTP was already sent recently. Please wait before resending.",
+      });
+    }
+
+    const otp = generateOTP();
+
+    otpStore.set(phone, { otp, expires: Date.now() + 300000 });
+
+    let formattedNumber = await formatPhoneNumber(phone.trim());
+
+    client.messages
+      .create({
+        body: `Your IPCS Global Verification code is ${otp}`,
+        from: "+12283356266",
+        to: formattedNumber,
+      })
+      .then((message) =>
+        res.status(200).json({ success: true, message: "OTP resent!" })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({ success: false, message: err.message });
+      });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
+
+app.post("/api/send-email2", (req, res) => {
+  const { name, email, phone, qualification, location, form } = req.body;
+
+  let emailHtml;
+  if (form === "Register") {
+    emailHtml = emailTemplate1
+      .replace("{{name}}", name)
+      .replace("{{email}}", email)
+      .replace("{{phone}}", phone)
+      .replace("{{qualification}}", qualification)
+      .replace("{{location}}", location);
+  } else if (form === "Offer") {
+    emailHtml = emailTemplate2
+      .replace("{{name}}", name)
+      .replace("{{phone}}", phone);
+  } else if (form === "Brochure") {
+    emailHtml = emailTemplate3
+      .replace("{{name}}", name)
+      .replace("{{email}}", email)
+      .replace("{{phone}}", phone);
+  }
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "ipcsglobalindia@gmail.com",
+      pass: "nnpd xhea roak mbbk",
+    },
+  });
+
+  const mailOptions = {
+    from: "ipcsglobalindia@gmail.com",
+    to: ["ipcsdeveloper@gmail.com"],
+    subject: "New Lead Form Submission on ",
+    html: emailHtml,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email:", error);
+      res.status(500).send("Error sending email");
+    } else {
+      console.log("Email sent:", info.response);
+      res.status(200).send("Email sent successfully");
+    }
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ util/template.js
@@ -0,0 +1,179 @@
+const emailTemplate1 = `
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+      background-color: #f4f4f4;
+    }
+    .container {
+      width: 100%;
+      max-width: 600px;
+      margin: 0 auto;
+      background-color: #ffffff;
+      border-radius: 8px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      overflow: hidden;
+    }
+    .header {
+      background-color: #4CAF50;
+      color: white;
+      padding: 20px;
+      text-align: center;
+    }
+    .content {
+      padding: 20px;
+    }
+    .content p {
+      margin: 0 0 20px;
+    }
+    .footer {
+      background-color: #f1f1f1;
+      color: #333333;
+      text-align: center;
+      padding: 10px;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <div class="header">
+      <h1>INDUSTRIAL AUTOMATION REGISTER FORM</h1>
+    </div>
+    <div class="content">
+      <p><strong>Name:</strong> {{name}}</p>
+      <p><strong>Email:</strong> {{email}}</p>
+      <p><strong>Phone:</strong> {{phone}}</p>
+      <p><strong>Qualification:</strong> {{qualification}}</p>
+    </div>
+    <div class="footer">
+      <p>&copy; 2024 IPCS GLOBAL. All rights reserved.</p>
+    </div>
+  </div>
+</body>
+</html>
+`;
+
+const emailTemplate2 = `
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+      background-color: #f4f4f4;
+    }
+    .container {
+      width: 100%;
+      max-width: 600px;
+      margin: 0 auto;
+      background-color: #ffffff;
+      border-radius: 8px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      overflow: hidden;
+    }
+    .header {
+      background-color: #4CAF50;
+      color: white;
+      padding: 20px;
+      text-align: center;
+    }
+    .content {
+      padding: 20px;
+    }
+    .content p {
+      margin: 0 0 20px;
+    }
+    .footer {
+      background-color: #f1f1f1;
+      color: #333333;
+      text-align: center;
+      padding: 10px;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <div class="header">
+      <h1>INDUSTRIAL AUTOMATION OFFER FORM</h1>
+    </div>
+    <div class="content">
+      <p><strong>Name:</strong> {{name}}</p>
+      <p><strong>Phone:</strong> {{phone}}</p>
+    </div>
+    <div class="footer">
+      <p>&copy; 2024 IPCS GLOBAL. All rights reserved.</p>
+    </div>
+  </div>
+</body>
+</html>
+`;
+
+const emailTemplate3 = `
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+      background-color: #f4f4f4;
+    }
+    .container {
+      width: 100%;
+      max-width: 600px;
+      margin: 0 auto;
+      background-color: #ffffff;
+      border-radius: 8px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+      overflow: hidden;
+    }
+    .header {
+      background-color: #4CAF50;
+      color: white;
+      padding: 20px;
+      text-align: center;
+    }
+    .content {
+      padding: 20px;
+    }
+    .content p {
+      margin: 0 0 20px;
+    }
+    .footer {
+      background-color: #f1f1f1;
+      color: #333333;
+      text-align: center;
+      padding: 10px;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <div class="header">
+      <h1>INDUSTRIAL AUTOMATION DOWNLOAD BROCHURE FORM</h1>
+    </div>
+    <div class="content">
+      <p><strong>Name:</strong> {{name}}</p>
+      <p><strong>Phone:</strong> {{phone}}</p>
+      <p><strong>Email:</strong> {{email}}</p>
+    </div>
+    <div class="footer">
+      <p>&copy; 2024 IPCS GLOBAL. All rights reserved.</p>
+    </div>
+  </div>
+</body>
+</html>
+`;
+
+module.exports = {
+  emailTemplate1,
+  emailTemplate2,
+  emailTemplate3,
+};
